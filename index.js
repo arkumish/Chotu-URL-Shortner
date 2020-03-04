314e12c8bcbd17129dbc8896f1455c93146a698d
armish24 <arpit.cse24@gmail.com>
2020-03-04 19:40:31 +0530

    backend url shortner working

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const URLSchema = require('./model')
+const URLSchema = require('./model');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = 8800;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Database connectivity
 const uri = 'mongodb://127.0.0.1:27017/urlShortner';
@@ -21,6 +25,31 @@ db.once('open', function () {
 
 app.all('/',(req,res)=>{
     res.send('hello world');
+});
+
+app.post('/shortURL',async (req,res)=>{
+  console.log(req.body);
+  let data  = {
+    FullURL : req.body.url
+  }
+  const newObj = new URLSchema(data);
+  const obj = await newObj.save();
+
+if (obj == null) { res.status(500).send(err); }
+
+  res.status(200).send(newObj);
+  
+})
+
+app.get('/:url', async (req,res)=>{
+
+  const urlobj = await URLSchema.findOne({ShortURL : req.params.url})
+  if( urlobj == null) return res.send(404);
+
+  urlobj.Click++;
+  urlobj.save();
+
+  res.redirect(urlobj.FullURL);
 })
 
 app.listen(port,()=>{
--- model.js
+++ model.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
+const shortid = require('shortid')
 
 const Schema = mongoose.Schema;
 
 let URLDetailSchema = new Schema({
-    FullURL = {type :String,required:true},
-    ShortURL = {type:String,required:true},
-    Click = {type:Number},
-    DateOfCreation = {type:Date, default:Date.now}
+    FullURL : {type:String,required:true},
+    ShortURL : {type:String,default:shortid.generate(),required:true},
+    Click : {type:Number,default:0},
+    DateOfCreation : {type:Date, default:Date.now}
 });
 
 module.exports = mongoose.model('URLschema',URLDetailSchema);
